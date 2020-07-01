8f75703d874104fc4ff6cab492054e7fd37a1169
Kallirroi <kallirroi.retzepi@gmail.com>
2020-07-01 17:38:23 -0400

    rendering pages and posts under the /entries/ URL

--- src/pages/index.js
+++ src/pages/index.js
@@ -12,42 +12,6 @@ function Home() {
       
       <h4> <Link to="/entries/" state={{ param: 'pages' }}>pages</Link> • <Link to="/entries/" state={{ param: 'posts' }}>posts</Link></h4>
       
-      <StaticQuery
-        query={graphql`
-          query {
-            pages: allWpPage {
-              nodes {
-                id
-                slug
-                date
-                title
-                uri
-              }
-            }
-            posts: allWpPost {
-              nodes {
-                id
-                slug
-                date
-                title
-                excerpt
-                uri
-              }
-            }
-          }
-        `}
-        render={data=>
-          data.posts.nodes.map((node) => (
-            <div key={node.id}>
-              <Link to={node.slug}>
-                {node.title}
-              </Link>              
-              <p>{node.date.slice(0,10)}</p>
-              <div dangerouslySetInnerHTML={{ __html: node.excerpt }} />
-            </div>
-          ))
-        }
-      />
     </Layout>
   )
 }
