66b4047ab30cce77334d3683723beb706d8fcb97
Kallirroi <kallirroi.retzepi@gmail.com>
2020-07-22 15:23:12 -0400

    correctly fetching tags in response to hardcoded value

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { gql, useQuery } from '@apollo/client'
 
 import {usePages} from "../hooks/usePages"
 import {usePosts} from "../hooks/usePosts"
@@ -10,17 +11,45 @@ import SEO from "../components/seo"
 import Filters from "../components/filters"
 import List from "../components/list"
 
+// The GraphQL query containing the search term, will be sent to Apollo
+const SEARCH_TAGS_QUERY = gql`
+  query SearchQuery($first: Int, $tagName: String!) {
+    posts(first: $first,where: {tag: $tagName}) {
+      nodes {
+        id
+        slug
+        title
+        date
+        excerpt
+      }
+    }
+    pages(first: $first,where: {tag: $tagName}) {
+      nodes {
+        id
+        slug
+        title
+        date
+        content
+      }
+    }
+  }
+`
+
 const Work = () => {
 	const pages = usePages()
 	const posts = usePosts()
   const categories = useCategories();
   const tags = useTags();
 
+  // GraphQL query to grab the posts/pages out of the specific Tag that has been selected
+  const {loading, error, data} = useQuery(SEARCH_TAGS_QUERY, {
+    variables: {first: 150, tagName: 'teaching'},
+  })
+  const searchResults = !loading ? [...data.posts.nodes, ...data.pages.nodes] : []
+  console.log(searchResults)
+
   // if no Tag is selected
   const items = [...pages,...posts]
-
-  // if Tags are selected (pseudocode)
-  // items.filter(item => item.selectedTag)
 
   return (
     <Browser>
