9537886bc004b8ee4448e7bd7ff62fc1e95215f5
Kallirroi <kallirroi.retzepi@gmail.com>
2020-08-08 17:10:37 -0400

    not rendering tags that occur only once

--- src/components/filters/tags.js
+++ src/components/filters/tags.js
@@ -4,26 +4,31 @@ import Checkbox from './checkbox'
 import TagsInfoBox from './tagsInfoBox'
 
 const Tags = ({tags, selectTags, clearTags, isTagMode}) => {
-
+	const topTags = tags.slice(0,20)
+  
   return (
-     <div className="no-scroll" 
-     	style={{
-	    textAlign: 'left',
-			overflowY: 'scroll',
-			overflowX: 'hidden',
-			height: '65vh',
-	     }}>
-	     	<TagsInfoBox tags={tags} clearTags={clearTags} isTagMode={isTagMode}/>
-	      {tags && tags.map((tag, index) => (
-					<Checkbox
-			      key={index}
-			      label={tag.name}
-			      isSelected={tag.checked}
-			      onCheckboxChange={selectTags}
-			    />
-	     	))}
-     </div>
-   )
+   <div 
+	   	style={{
+		    textAlign: 'left',
+				overflowY: 'scroll',
+				overflowX: 'hidden',
+				maxHeight: '45vh'
+	    }}>
+     	<TagsInfoBox tags={tags} clearTags={clearTags} isTagMode={isTagMode}/>
+      
+      {tags && tags.map((tag, index) => (
+				<Checkbox
+		      key={index}
+		      label={tag.name}
+		      count={tag.pages.nodes.length + tag.posts.nodes.length}
+		      isSelected={tag.checked}
+		      onCheckboxChange={selectTags}
+		    />
+     	))}
+
+
+   </div>
+  )
 }
 
 export default Tags 
--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -6,36 +6,20 @@ export const getMonthName = (index) => {
   return monthNames[index-1]
 }
 
-// When we first land on a /category endpoint, we want the Tag list to automatically display Tags that are associated with this category's content. We can't do that dynamically with Apollo and we can't use a Gatsby static Query since it doesn't accept variables. We have to construct the [tags] array manually, by extracting them out of the deeply nested 'category' variable. 
-export const extractTags = (initial) => {
-  return initial
-         	.filter(hasTags=>hasTags.tags && hasTags.tags.nodes.length>0) // filter for elements that contain non-zero Tag arrays
-         	.map(item=> { 
-            // transform the incoming array into an object with 3 key-value pairs => {checked: bool, slug: string, id: string}, which we need for Tag UX
-            let item_tags = item.tags.nodes.map(tag => {
-                              return {checked: false, slug: tag.slug, name: tag.name, id: tag.id}
-                            }) 
-            return item_tags
-         })
-         .flat(2) // flattens the incoming array of arrays (depth=2)
-         .filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i) // checks for uniqueness based on id, seen here https://stackoverflow.com/a/56757215 
-         .sort( (a, b) => a.id.localeCompare(b.id, 'en', {'sensitivity': 'base'})) // sorts the incoming array of objects alphabetically by 'name', seen here https://stackoverflow.com/a/58958381
-}
-
 export const sortTags = (tags) => {
    return tags
             .filter(tag => tag.name.length > 0) // filter for nodes which have tags
-            .filter(hasTags => hasTags.pages.nodes.length>0 || hasTags.posts.nodes.length>0) // filter for nodes the Posts or Pages nodes have tags
+            .filter(hasTags => hasTags.pages.nodes.length > 0 || hasTags.posts.nodes.length > 0) // filter for nodes the Posts or Pages nodes have tags
             .sort((a, b) => a.posts.nodes.length + a.pages.nodes.length < b.posts.nodes.length + b.pages.nodes.length ? 1 : -1) // sort DESC by the number of tags across both Posts and Pages
-            .map(obj=> ({ ...obj, checked: false })) // modify the incoming array by inserting a {checked: true|false} field to every object, which is used for selecting Tags
+            .map(obj=> ({ ...obj, checked: false })) // modify the incoming array by inserting a {checked: true | false} field to every object, which is used for selecting Tags
 }
 
 export const sortByDate = (array) => {
   return array.sort((a,b)=> {
-    if (a.date && b.date) {
-      return parseInt(b.date.slice(0,4)) - parseInt(a.date.slice(0,4))
-    }
-  })
+            if (a.date && b.date) {
+              return parseInt(b.date.slice(0,4)) - parseInt(a.date.slice(0,4))
+            }
+          })
 }
 
 export const extractSearchResults = (array) => {
@@ -55,16 +39,36 @@ export const getRelated = (tags, title) => {
   let related = [...relatedPages, ...relatedPosts]
                 .filter((v,i,a)=>a.findIndex(t=>(t.title === v.title))===i)
                 .filter(item => item.title !== title)
+  
   return related
 }
 
 function getRandomSubarray(arr, size) {
-    var shuffled = arr.slice(0), i = arr.length, temp, index;
-    while (i--) {
-        index = Math.floor((i + 1) * Math.random());
-        temp = shuffled[index];
-        shuffled[index] = shuffled[i];
-        shuffled[i] = temp;
-    }
-    return shuffled.slice(0, size);
+  let shuffled = arr.slice(0), i = arr.length, temp, index;
+  while (i--) {
+      index = Math.floor((i + 1) * Math.random());
+      temp = shuffled[index];
+      shuffled[index] = shuffled[i];
+      shuffled[i] = temp;
+  }
+  return shuffled.slice(0, size);
+}
+
+// 
+// NOT CURRENTLY IN USE
+// 
+// When we first land on a /category endpoint, we want the Tag list to automatically display Tags that are associated with this category's content. We can't do that dynamically with Apollo and we can't use a Gatsby static Query since it doesn't accept variables. We have to construct the [tags] array manually, by extracting them out of the deeply nested 'category' variable. 
+export const extractTags = (initial) => {
+  return initial
+           .filter(hasTags=>hasTags.tags && hasTags.tags.nodes.length>0) // filter for elements that contain non-zero Tag arrays
+           .map(item=> { 
+            // transform the incoming array into an object with 3 key-value pairs => {checked: bool, slug: string, id: string}, which we need for Tag UX
+            let item_tags = item.tags.nodes.map(tag => {
+                              return {checked: false, slug: tag.slug, name: tag.name, id: tag.id}
+                            }) 
+            return item_tags
+         })
+         .flat(2) // flattens the incoming array of arrays (depth=2)
+         .filter((v,i,a)=>a.findIndex(t=>(t.id === v.id))===i) // checks for uniqueness based on id, seen here https://stackoverflow.com/a/56757215 
+         .sort( (a, b) => a.id.localeCompare(b.id, 'en', {'sensitivity': 'base'})) // sorts the incoming array of objects alphabetically by 'name', seen here https://stackoverflow.com/a/58958381
 }
