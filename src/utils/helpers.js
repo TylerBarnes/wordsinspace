edcf7b2c7067c0d4f05d23497a19f849a254cd5a
Kallirroi <kallirroi.retzepi@gmail.com>
2020-08-08 14:57:09 -0400

    checking typeof window

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -50,8 +50,6 @@ export const extractSearchResults = (array) => {
 
 export const getRelated = (tags, title) => {
   let randomTagSelection = getRandomSubarray(tags.nodes, 3)
-  console.log(randomTagSelection)
-
   let relatedPages = randomTagSelection.map(tag => getRandomSubarray(tag.pages.nodes, 2)).flat(2)
   let relatedPosts = randomTagSelection.map(tag => getRandomSubarray(tag.posts.nodes, 2)).flat(2)
   let related = [...relatedPages, ...relatedPosts]
