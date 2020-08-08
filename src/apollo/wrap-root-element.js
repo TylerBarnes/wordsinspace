4c7948d517632782bc9dcad8fa2cf19f2d086809
Kallirroi <kallirroi.retzepi@gmail.com>
2020-08-08 11:51:28 -0400

    apollo file back!

---
+++ src/apollo/wrap-root-element.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import fetch from "isomorphic-fetch"
+
+const httpLink = createHttpLink({
+    uri: process.env.NODE_ENV === `development`
+        ? `http://localhost:8888/test/graphql`
+        : `https://importii.wordsinspace.net/graphql`
+});
+
+const client = new ApolloClient({
+    fetch,
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+);
