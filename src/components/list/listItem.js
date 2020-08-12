7dd64f00aeab362e94f3b461274c0effcc86201c
Sam Ghantous <samghantous@gmail.com>
2020-08-12 20:13:36 +0100

    ugh done balancing listtag position between browsers

--- src/components/list/listItem.js
+++ src/components/list/listItem.js
@@ -70,7 +70,7 @@ const ListItem = ({item, isTagMode, invertedTheme, mobileList, listWidth, listTi
             flexDirection: 'row',
             justifyContent: 'space-evenly',
             alignItems: 'flex-start',
-            marginLeft: mobileList ? '0' :'40px',
+            margin: mobileList ? '10px 0 0 0' :'1vh 0 0 2vh',
           }}>
           {date && <ListDate date={date} invertedTheme={invertedTheme} />}
           {category && category !== 'Uncategorized' && <ListCategory category={category.toLowerCase()} />}
@@ -81,7 +81,7 @@ const ListItem = ({item, isTagMode, invertedTheme, mobileList, listWidth, listTi
         <div
           style={{
             width: listTitleWidth,
-            margin: mobileList ? '5px 0' : '30px 0 40px 0',
+            margin: mobileList ? '15px 0' : '2vh 0 4vh 0',
             paddingLeft: mobileList ? '0' : '20px',
           }}>
           <Link
