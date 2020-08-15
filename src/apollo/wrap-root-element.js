1129da2c437ffcf7d333d7fb89cba90bdaec0cfd
Sam Ghantous <samghantous@gmail.com>
2020-08-15 16:38:51 -0400

    fixed character encoding bug on mainpage

---
+++ src/apollo/wrap-root-element.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
+import fetch from "isomorphic-fetch"
+
+const httpLink = createHttpLink({
+    uri: process.env.NODE_ENV === `development`
+        ? `https://final.wordsinspace.net/graphql`//`http://localhost:8888/shannon11/graphql`
+        : `https://importiv.wordsinspace.net/graphql`
+});
+
+const client = new ApolloClient({
+    fetch,
+    link: httpLink,
+    cache: new InMemoryCache(),
+});
+
+export const wrapRootElement = ({ element }) => (
+  <ApolloProvider client={client}>{element}</ApolloProvider>
+);
