9b1f67f020c821c71618dffaa6e0b1dd525f10f9
Kallirroi <kallirroi.retzepi@gmail.com>
2021-03-13 13:44:46 -0500

    trying out XML generator on build

---
+++ rss-gen.js
@@ -0,0 +1,29 @@
+const fs = require('fs');
+const path = require('path');
+const axios = require('axios');
+
+const getXML = async () => {
+  const response = await axios({
+    method: 'get',
+    url: 'https://icd.wordsinspace.net/',
+    headers: { 'X-Requested-With': 'XMLHttpRequest' },
+  });
+
+  return response.data;
+};
+
+async function generateRSS() {
+  const XMLfile = await getXML();
+  const staticOutputPath = path.join(process.cwd(), 'public');
+
+  fs.writeFile(`${staticOutputPath}/rss.xml`, XMLfile, err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('File written successfully');
+    }
+  });
+}
+
+// kick it all off
+generateRSS();
