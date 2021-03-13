f0026ca2346d39acdc9155bb6e419c6dfd5959c2
Kallirroi <kallirroi.retzepi@gmail.com>
2021-03-13 17:55:11 -0500

    clean XML

--- rss-gen.js
+++ rss-gen.js
@@ -1,27 +1,32 @@
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
+var parseString = require('xml2js').parseString;
 
 const getXML = async () => {
   const response = await axios({
     method: 'get',
-    url: 'https://icd.wordsinspace.net/',
-    headers: { 'X-Requested-With': 'XMLHttpRequest' },
+    url: 'https://icd.wordsinspace.net/feed/rss2',
+    headers: { 'Content-Type': 'text/xml' },
   });
-  return response.data;
+  return response.data
 };
 
 async function generateRSS() {
-  const XMLfile = await getXML();
+  const XMLfile = await getXML()
   const staticOutputPath = path.join(process.cwd(), './static/');
+  parseString(XMLfile, (err, result) => {
+    fs.writeFile(`${staticOutputPath}/rss.xml`, XMLfile, err => {
+      if (err) {
+        console.log(err);
+      } else {
+        console.log('File written successfully');
+      }
+    });
 
-  fs.writeFile(`${staticOutputPath}/rss.xml`, XMLfile, err => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('File written successfully');
-    }
   });
+
+
 }
 
 // kick it all off
