3fa2a76e2f9ebe3ff70d900551adeb07780584a0
Kallirroi <kallirroi.retzepi@gmail.com>
2021-03-13 18:49:05 -0500

    piecing together a new XML file - WIP

--- rss-gen.js
+++ rss-gen.js
@@ -3,23 +3,90 @@ const path = require('path');
 const axios = require('axios');
 var parseString = require('xml2js').parseString;
 
-const getXML = async () => {
+const fetchInitialXML = async () => {
   const response = await axios({
     method: 'get',
     url: 'https://icd.wordsinspace.net/feed/rss2',
-    headers: { 'Content-Type': 'text/xml' },
-  });
+    headers: { 'Content-Type': 'application/xml; charset=utf-8' },
+  })
   return response.data
 };
 
-async function generateRSS() {
-  const XMLfile = await getXML()
-  const staticOutputPath = path.join(process.cwd(), './static/');
-  // parseString(XMLfile, (err, result) => {
-  //   console.log(result)
-  // });
+const createPostRSSfeed = (XMLfile) => {
+  let rssItemsXml, lastBuildDate, language, description
+  parseString(XMLfile, (err, result) => {
+    const data = result.rss.channel[0]
+    const { item } = data
+    lastBuildDate = data.lastBuildDate
+    language = data.language
+    description = data.description
+    description = data.description
 
-  fs.writeFile(`${staticOutputPath}/rss.xml`, XMLfile, err => {
+    // iterating over posts/pages
+    item.map(i => {
+      const { title, link, pubDate, category, guid, description, content } = i
+      const modifiedURL = 'https://wordsinspace.net/'
+
+      rssItemsXml += `
+        <item>
+          <title><![CDATA[${title}]]></title>
+          <link>${modifiedURL}</link>
+          <pubDate>${pubDate}</pubDate>
+          <guid isPermaLink="false">${guid}</guid>
+          <category>${category}</category>
+          <description>
+          <![CDATA[${description}]]>
+          </description>
+          <content:encoded>
+            <![CDATA[${content}]]>
+          </content:encoded>
+      </item>`
+    })
+  });
+  return {
+    rssItemsXml, 
+    lastBuildDate, 
+    language, 
+    description,
+  };
+
+}
+
+const assembleRSSfragments = (XMLfile) => {
+  const { 
+    rssItemsXml,
+    lastBuildDate,
+    language,
+    description } = createPostRSSfeed(XMLfile)
+
+  return `<?xml version="1.0" ?>
+      <rss
+        xmlns:dc="http://purl.org/dc/elements/1.1/"
+        xmlns:content="http://purl.org/rss/1.0/modules/content/"
+        xmlns:atom="http://www.w3.org/2005/Atom"
+        xmlns:sy="http://purl.org/rss/1.0/modules/syndication/"
+        xmlns:slash="http://purl.org/rss/1.0/modules/slash/"
+        version="2.0"
+      >
+        <channel>
+            <title><![CDATA[Words In Space]]></title>
+            <link>https://wordsinspace.net/</link>
+            <description>
+              <![CDATA[${description}]]>
+            </description>
+            <language>${language}</language>
+            <lastBuildDate>${lastBuildDate}</lastBuildDate>
+            ${rssItemsXml}
+        </channel>
+      </rss>`
+}
+
+async function generateRSS() {
+  const XMLfile = await fetchInitialXML()
+  const parsedXML = assembleRSSfragments(XMLfile)
+  const staticOutputPath = path.join(process.cwd(), './static/');
+
+  fs.writeFile(`${staticOutputPath}/rss.xml`, parsedXML, err => {
     if (err) {
       console.log(err);
     } else {
